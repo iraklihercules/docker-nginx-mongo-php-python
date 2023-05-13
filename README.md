e87d29cb2d4d435acb923c04016d0d41c3d2f1f8
Irakli <irakli.alaverdashvili@hotmail.com>
2023-05-13 03:24:42 +0200

    Using MongoDB in PHP container

--- README.md
+++ README.md
@@ -5,22 +5,38 @@ This is a basic Docker template with `Nginx` `PHP-FPM` `MongoDB` and `Mongo Expr
 * [http://localhost:8080](http://localhost:8080) - Website
 * [http://localhost:8081](http://localhost:8081) - Mongo Express
 
+### 1. What the app does ###
 
-### 1. Software requirements ###
+1. Creates a collection
+2. Inserts 2 users
+3. Gets one user back
+4. Displays the user
+
+```
+Array
+(
+    [username] => John Doe
+    [email] => john.doe@example.com
+    [created_on] => {creation_date}
+)
+```
+
+
+### 2. Software requirements ###
 
 * [Git](https://git-scm.com/)
 * [Docker](https://www.docker.com/)
 * [Docker Compose](https://docs.docker.com/compose/)
 
 
-### 2. How to run the project ###
+### 3. How to run the project ###
 
 * Clone the repository
 * Enter the project directory
 * Run `docker-compose up -d` or `make up` (Make command may require installation).
 
 
-### 3. Recipes ###
+### 4. Recipes ###
 
 ```bash
 make up  # Build and run containers
@@ -33,7 +49,7 @@ make mongo-shell  # Open the MongoDB container's shell
 make db-clear  # Delete data in the database (requires containers restart)
 ```
 
-### 4. Using MongoDB Clients ###
+### 5. Using MongoDB Clients ###
 
 To connect to the database with any desktop client, use this details:
 ```yaml
--- app/src/index.php
+++ app/src/index.php
@@ -1,3 +1,54 @@
 <?php
 
-echo 'It works!';
+use MongoDB\Driver\Query;
+use MongoDB\Driver\Manager;
+use MongoDB\Driver\BulkWrite;
+use MongoDB\BSON\UTCDateTime;
+
+function getUsersFromDB(Manager $manager, string $collection): array
+{
+    $filter = ['username' => 'John Doe'];
+    $options = ['limit' => 1];
+    $query = new Query($filter, $options);
+    $cursor = $manager->executeQuery($collection, $query);
+    return $cursor->toArray();
+}
+
+const DATABASE = 'hercules_database';
+const USERNAME = 'test';
+const PASSWORD = 'test';
+
+$manager = new Manager('mongodb://mongo:27017', ['username' => USERNAME, 'password' => PASSWORD]);
+$collection = DATABASE.'.User';
+
+/* Get users */
+$users = getUsersFromDB($manager, $collection);
+
+if (empty($users)) {
+    /* Insert users if the result is empty */
+    $bulkWrite = new BulkWrite;
+    $bulkWrite->insert([
+        'username' => 'John Doe',
+        'email' => 'john.doe@example.com',
+        'createdAt' => new UTCDateTime,
+    ]);
+    $bulkWrite->insert([
+        'username' => 'Foo Bar',
+        'email' => 'foo.bar@example.com',
+        'createdAt' => new UTCDateTime,
+    ]);
+    $manager->executeBulkWrite($collection, $bulkWrite);
+
+    /* Get users again */
+    $users = getUsersFromDB($manager, $collection);
+}
+
+/* Display the first user */
+echo '<pre>';
+$user = $users[0];
+print_r([
+    'username' => $user->username,
+    'email' => $user->email,
+    'created_at' => $user->createdAt->toDateTime()->format('Y-m-d H:i'),
+]);
+echo '</pre>';
